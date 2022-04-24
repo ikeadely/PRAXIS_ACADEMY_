aea3c247927995590b9b0f41601f853367362e46
Your Name <you@example.com>
2022-04-24 13:45:32 +0700

    update latihan 7

--- NOVICE_/Minggu_02/day-4/penjelasan nomor 7/DI_di_Python_dari_perspektif_java.py
+++ NOVICE_/Minggu_02/day-4/penjelasan nomor 7/DI_di_Python_dari_perspektif_java.py
@@ -1,9 +1,36 @@
 # A Quick Detour on Dependency Injection
 
+# pip install injector
+# Requirement already satisfied: injector in ./.local/lib/python3.8/site-packages (0.19.0)
+# Requirement already satisfied: typing-extensions>=3.7.4; python_version < "3.9" in ./.local/lib/python3.8/site-packages (from injector) (4.2.0)
+
+# class Api:
+#     def fetch_remote_data(self):
+#         print('Api called')
+#         return 42
+
+# class BusinessLogic:
+#     def __init__(self, api: Api):
+#         self.api = api
+
+#     def do_stuff(self):
+#         api_result = self.api.fetch_remote_data()
+#         print(f'the api returned a result: {api_result}')
+
+# if __name__ == '__main__':
+#     api = Api()
+#     lodic = BusinessLogic(api=api)
+
+#     print(logic.do_stuff())
+
+from msvcrt import getwch
+
+
 class Api:
     def fetch_remote_data(self):
         print('Api called')
         return 42
+
 
 class BusinessLogic:
     def __init__(self, api: Api):
@@ -13,24 +40,27 @@ class BusinessLogic:
         api_result = self.api.fetch_remote_data()
         print(f'the api returned a result: {api_result}')
 
-if __name__ == '__main__':
-    api = Api()
-    lodic = BusinessLogic(api=api)
 
-    print(logic.do_stuff())
+# if __name__ == '__main__':
+#     injector = injector(AppModule())
 
-class AppModule(Module):
+#     logic = injector.get(BusinessLogic)
+#     logic.do_stuff()
 
-    @singleton
-    @provider
-    def provide_business_logic(self, api: Api) -> BusinessLogic:
-        return BusinessLogic(api=api)
+class TestApi(Api):
+    def fetch_remote_data(self):
+        print('Demo Api called')
+        return 24
 
-    @singleton
-    @provider
-    def provide_api(self) -> Api:
-        # there is no complex logic in our case,
-        # but you can use this method to hide the complexity of initial 
-        configuration
-        # e.g. when instantiating a particular DB connector.
-        return Api()
+# if __name__ == '__main__':
+#     real_injector = Injector(AppModule())
+#     test_injector = Injector([AppModule(), TestAppModule()])
+
+#     real_logic = real_injector.get(BusinessLogic)
+#     real_logic.do_stuff()
+
+#     test_logic = test_injector.get(BusinessLogic)
+#     test_logic.do_stuff()
+
+# 
+# ^^ gtw
