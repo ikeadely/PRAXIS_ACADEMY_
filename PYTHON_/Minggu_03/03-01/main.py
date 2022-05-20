8cb789cbd12e13a30eaee7aa9d3308c544946f79
Your Name <you@example.com>
2022-05-20 16:31:50 +0700

    flask app

--- PYTHON_/Minggu_03/03-01/main.py
+++ PYTHON_/Minggu_03/03-01/main.py
@@ -1,14 +1,18 @@
-import pymongo
 from dotenv import load_dotenv, find_dotenv
 import os
 import pprint
 load_dotenv(find_dotenv())
 from pymongo import MongoClient
 
-
 password = os.environ.get("MONGODB_PWD")
 
-connection_string =f"mongodb+srv://adel:{password}@cluster0.fjcgf.mongodb.net/?retryWrites=true&w=majority"
-
+connection_string =f"mongodb+srv://adel:{password}@cluster0.gds4d.mongodb.net/test"
 client = MongoClient(connection_string)
 
+
+# memasukkan database
+mydb = client["mydb"]
+mycol = mydb["riwayat"]
+
+datalist = [{'nama':'adel', 'umur':'22', 'alamat':'trangkil'}, {'nama':'cha', 'umur':'21', 'alamat':'jepara'}, {'nama':'nis', 'umur':'24', 'alamat':'kajen'}, {'nama':'adi', 'umur':'25', 'alamat':'tayu'}, {'nama':'uut', 'umur':'30', 'alamat':'rembang'}, {'nama':'sya', 'umur':'29', 'alamat':'rembang'}, {'nama':'don', 'umur':'40', 'alamat':'jogja'}]
+mycol.insert_many(datalist)
