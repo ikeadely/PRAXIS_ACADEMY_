8f6ab44c6403773546d82b1ad8ab4f97ce30d98f
Your Name <you@example.com>
2022-05-26 16:53:44 +0700

    2605

--- PYTHON_/Minggu_03/03-02/contoh_flask.py
+++ PYTHON_/Minggu_03/03-02/contoh_flask.py
@@ -8,3 +8,16 @@ def hello():
 
 if __name__ == "__main__":
     app.run()
+
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def profil():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
