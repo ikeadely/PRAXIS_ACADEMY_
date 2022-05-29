ee968fa2fb13a2e148eaf9ac639f2e520535650b
ikeadely <dwike.adelya@gmail.com>
2022-05-29 19:32:55 +0700

    flask latihan

---
+++ PYTHON_/Minggu_03/03-02/flask/Routing/route.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello ADel'
+
+def hello_world():
+    return 'Hello ADel, apakabar hari ini'
+app.add_url_rule('/', 'hello', hello_world)
+
+if __name__ == '__main__':
+    app.run()
+
+#blm
