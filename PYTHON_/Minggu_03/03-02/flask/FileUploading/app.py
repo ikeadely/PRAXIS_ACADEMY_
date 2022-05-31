e218000919f25614dc7c3a4e9287f6e6e011e64b
ikeadely <dwike.adelya@gmail.com>
2022-05-31 07:58:19 +0700

    contoh integrasi jinja2 ke flask

--- PYTHON_/Minggu_03/03-02/flask/FileUploading/app.py
+++ PYTHON_/Minggu_03/03-02/flask/FileUploading/app.py
@@ -1,17 +1,24 @@
+import os
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
-@app.route('/upload')
+@app.route('/')
 def upload_file():
    return render_template('upload.html')
 	
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file1():
+   file = request.files['file']
+   filename = secure_filename(file.filename)
+   file.save(os.path.join('upload', filename))
+   return "successfully upload file"
+
+   # if request.method == 'POST':
+   #    f = request.files['file']
+   #    f.save(secure_filename(f.filename))
+   #    return 'file uploaded successfully'
 		
 if __name__ == '__main__':
    app.run(debug = True)
