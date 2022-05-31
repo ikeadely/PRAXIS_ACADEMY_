1a72adf5202f42285800ef24716f26c5d5fab16e
ikeadely <dwike.adelya@gmail.com>
2022-05-31 10:46:41 +0700

    pengujian aplikasi flask

---
+++ PYTHON_/Minggu_03/03-04/test.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    unittest.main()
+
+
+# ditest
+# python -m unittest test
+
+# hasil
+
+# ----------------------------------------------------------------------
+# Ran 0 tests in 0.000s
+
+# OK
