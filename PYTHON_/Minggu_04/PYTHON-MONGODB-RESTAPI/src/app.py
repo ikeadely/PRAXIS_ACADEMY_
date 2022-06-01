b39381ad66491ff0020f178d523582935115fef7
ikeadely <dwike.adelya@gmail.com>
2022-06-02 01:38:38 +0700

    python flask & mongodb REST API

--- PYTHON_/Minggu_04/PYTHON-MONGODB-RESTAPI/src/app.py
+++ PYTHON_/Minggu_04/PYTHON-MONGODB-RESTAPI/src/app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, request, 
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['MONGO_URI']='mongodb://localhost/python'
+app.config['MONGO_URI']='mongodb://localhost/pythonmongodb'
+mongo = PyMongo(app)
 
+@app.route('/user', methods=['POST'])
+def create_user():
+    # receiving data
+    print(request.json)
+
+    return {'message':'received'}
 
 if __name__== "__main__":
     app.run(debug=True) 
