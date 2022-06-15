5d87100739dd1959f98b511d8cfacd3271fa0c8d
ikeadely <dwike.adelya@gmail.com>
2022-06-15 16:10:46 +0700

    postgres

--- PYTHON_/Minggu_04/postgres/create_table.py
+++ PYTHON_/Minggu_04/postgres/create_table.py
@@ -34,6 +34,26 @@ def create_tables():
         )
         """)
 
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
 if __name__ == '__main__':
     create_tables()
 
